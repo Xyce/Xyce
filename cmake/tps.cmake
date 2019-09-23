1aac4b1f581e1ae086f889c6ca43f8fb279d8bf0
Jason Verley <jcverle@sandia.gov>
2019-09-23 17:14:30 -0600

    Refine Trlinos probing messages and add CURL probe
    This doesn't significantly change the way Trilinos is probed, but cleans
    up the messages. It also adds some informational probes for the user
    (that apply to optional linear solver features of Xyce).
    
    I also added an inital probe for CURL, using the default CMake probe. It
    may need to be refined over time.

--- cmake/tps.cmake
+++ cmake/tps.cmake
@@ -42,7 +42,7 @@
 #    `Trilinos/cmake/doc/export_system/Finding_Trilinos.txt`
 
 # Trilinos recommends it be found BEFORE project() is called.
-# We *may* be able to put the search here, BUT some important (and subtle?)
+# One *may* be able to put the search here, BUT some important (and subtle?)
 # aspects setup by project() may be missing. 
 
 # MPI check
@@ -50,27 +50,29 @@ message("-- Checking if MPI is enabled in Trilinos")
 list(FIND Trilinos_TPL_LIST MPI MPI_Enabled)
 if (MPI_Enabled GREATER -1)
      message("-- Checking if MPI is enabled in Trilinos - MPI enabled")
-     set(Xyce_PARALLEL_MPI TRUE)
+     set(Xyce_PARALLEL_MPI TRUE) # CACHE BOOL "Build Xyce with MPI enabled")
 
-     # For MPI builds, Isorropia and Zoltan are REQUIRED.
+     # For MPI builds, Isorropia and Zoltan are REQUIRED
      message("-- Looking for Isorropia in Trilinos")
      list(FIND Trilinos_PACKAGE_LIST Isorropia Isorropia_FOUND)
      if ( Isorropia_FOUND GREATER -1)
           set(Xyce_USE_ISORROPIA TRUE)
           message("-- Looking for Isorropia in Trilinos - found")
      else ()
-          message("-- Looking for Isorropia in Trilinos - not found\n")
-          message(FATAL_ERROR "-- FATAL ERROR: Isorropia is required for MPI parallel builds.\n"
-                              "                Fix the Trilinos build, and try again.")
+          message("-- Looking for Isorropia in Trilinos - not found")
+          message(FATAL_ERROR "Isorropia is required for MPI parallel builds."
+               "Enable the following in the Trilinos build, and try again:"
+               "  -D Trilinos_ENABLE_Isorropia=ON")
      endif()
      message("-- Looking for Zoltan in Trilinos")
      list(FIND Trilinos_PACKAGE_LIST Zoltan Zoltan_FOUND)
-     if (NOT (Zoltan_FOUND GREATER -1))
-          message("-- Looking for Zoltan in Trilinos - not found\n")
-          message(FATAL_ERROR "-- FATAL ERROR: Zoltan is required for MPI parallel builds.\n"
-                              "                Fix the Trilinos build, and try again.")
+     if (Zoltan_FOUND LESS 0)
+          message("-- Looking for Zoltan in Trilinos - not found")
+          message(FATAL_ERROR "Zoltan is required for MPI parallel builds."
+               "Enable the following in the Trilinos build, and try again:"
+               "  -D Trilinos_ENABLE_Zoltan=ON")
      endif()
-     message("-- Looking for Isorropia in Trilinos - found")
+     message("-- Looking for Zoltan in Trilinos - found")
 else()
      message("-- Checking if MPI is enabled in Trilinos - MPI not enabled")
      set(Xyce_PARALLEL_MPI FALSE)
@@ -87,28 +89,65 @@ LIST(REVERSE Trilinos_TPL_LIBRARIES)
 
 add_library(trilinos INTERFACE IMPORTED GLOBAL)
 
-# ??? IS AMD REQUIRED, OR NOT ???
-# If it is, then the "ifdef" needs to be fixed!!!
-list(FIND Trilinos_TPL_LIST AMD AMD_IN_Trilinos)
-if (AMD_IN_Trilinos GREATER -1)
-     message("AMD found in Trilinos")
-     add_library(AMD INTERFACE IMPORTED GLOBAL)
-     set(Xyce_AMD TRUE)
-else()
-     message(FATAL_ERROR "-- FATAL ERROR: AMD not available via Trilinos.\n"
-                         "--              Fix the Trilinos build, and try again.\n")
-endif()
-
 list(FIND Trilinos_TPL_LIST BLAS BLAS_IN_Trilinos)
 list(FIND Trilinos_TPL_LIST LAPACK LAPACK_IN_Trilinos)
 message("-- Looking for BLAS and LAPACK in Trilinos")
 if ((BLAS_IN_Trilinos GREATER -1) AND (LAPACK_IN_Trilinos GREATER -1))
      message("-- Looking for BLAS and LAPACK in Trilinos - found")
 else ()
-     message("-- Looking for BLAS and LAPACK in Trilinos - not found\n")
-     message(FATAL_ERROR "-- FATAL ERROR: BLAS and LAPACK not available via Trilinos.\n"
-                         "--              Fix the Trilinos build, and try again.")
+     message("-- Looking for BLAS and LAPACK in Trilinos - not found")
+     message(FATAL_ERROR "BLAS and LAPACK are not available via Trilinos."
+          "Enable the following in the Trilinos build, and try again."
+          "  -D TPL_ENABLE_BLAS=ON"
+          "  -D TPL_ENABLE_LAPACK=ON")
 endif ()
+
+message("-- Looking for ParMETIS in Trilinos")
+list(FIND Trilinos_TPL_LIST ParMETIS ParMETIS_IN_Trilinos)
+if (ParMETIS_IN_Trilinos GREATER -1)
+     message("-- Looking for ParMETIS in Trilinos - found")
+     # add_library(ParMETIS INTERFACE IMPORTED GLOBAL)
+     set(Xyce_USE_PARMETIS TRUE) # CACHE BOOL "Enables the ParMETIS linear solver")
+else()
+     message("-- Looking for ParMETIS in Trilinos - not found")
+endif()
+
+message("-- Looking for AMD in Trilinos")
+list(FIND Trilinos_TPL_LIST AMD AMD_IN_Trilinos)
+if (AMD_IN_Trilinos GREATER -1)
+     message("-- Looking for AMD in Trilinos - found")
+     # add_library(AMD INTERFACE IMPORTED GLOBAL)
+     set(Xyce_AMD TRUE) # CACHE BOOL "Enables the option of AMD ordering for the linear solver")
+else()
+     message("-- Looking for AMD in Trilinos - not found")
+endif()
+
+message("-- Looking for PARDISO_MKL in Trilinos")
+list(FIND Trilinos_TPL_LIST PARDISO_MKL PARDISO_IN_Trilinos)
+if (PARDISO_IN_Trilinos GREATER -1)
+     message("-- Looking for PARDISO_MKL in Trilinos - found")
+     #     add_library(PARDISO_MKL INTERFACE IMPORTED GLOBAL)
+else()
+     message("-- Looking for PARDISO_MKL in Trilinos - not found")
+endif()
+
+message("-- Looking for SuperLU in Trilinos")
+list(FIND Trilinos_TPL_LIST SuperLU SuperLU_IN_Trilinos)
+if (SuperLU_IN_Trilinos GREATER -1)
+     message("-- Looking for SuperLU in Trilinos - found")
+     #add_library(SuperLU INTERFACE IMPORTED GLOBAL)
+else()
+     message("-- Looking for SuperLU in Trilinos - not found")
+endif()
+
+message("-- Looking for SuperLUDist in Trilinos")
+list(FIND Trilinos_TPL_LIST SuperLUDist SuperLUDist_IN_Trilinos)
+if (SuperLUDist_IN_Trilinos GREATER -1)
+     message("-- Looking for SuperLUDist in Trilinos - found")
+     #add_library(SuperLUDist INTERFACE IMPORTED GLOBAL)
+else()
+     message("-- Looking for SuperLUDist in Trilinos - not found")
+endif()
 
 # Follow something like this pattern for the optional Trilinos packages
 # Set optional dependency in MyApp on Epetra package:
@@ -144,8 +183,8 @@ else ()
 endif ()
 
 if (Xyce_REACTION_PARSER)
-   find_package(BISON 2.3 REQUIRED)
-   # The 2.3 specifies the minimum version.  That is ok at the moment, as Bison
+   find_package(BISON 2.4 REQUIRED)
+   # The 2.4 specifies the minimum version.  That is ok at the moment, as Bison
    # has been functional for many versions (through 3.4 at the time of this
    # writing).  Historically, though, new versions have had backward
    # incompatibility issues.  If that occurs again, the BISON_VERSION variable
@@ -153,3 +192,16 @@ if (Xyce_REACTION_PARSER)
    find_package(FLEX REQUIRED)
    #[[ "Define YYTEXT_POINTER if yytext defaults to 'char *' instead of 'char'"]]
 endif ()
+
+if (Xyce_USE_CURL)
+     if (Xyce_TRACKING_URL)
+          message("-- Looking for CURL")
+          find_package(CURL REQUIRED)
+          message("-- Looking for CURL - found")
+     else()
+          message("-- WARNING: Xyce_USE_CURL is TRUE, but no URL is supplied in Xyce_TRACKING_URL."
+                  "            Setting Xyce_USE_CURL to FALSE.")
+          set(Xyce_USE_CURL FALSE)
+     endif()
+endif()
+
