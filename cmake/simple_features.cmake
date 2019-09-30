54d80150d9123c254df4414bef0543046c5333d5
Jason Verley <jcverle@sandia.gov>
2019-09-30 17:16:55 -0600

    Added CMake logic for inclusion of optional dirs
    The optional directories should be included when they are there, by
    default; but options can turn off whether they get included. This added
    logic should handle all scenarios correctly. It probably needs a little
    more testing than I have given it, though.
    
    I put them in the simple_features.cmake file for lack of a better
    location. The file should probably be renamed.
    
    Finally, I made some formatting changes in the
    src/DeviceModelPKG/CMakeLists.txt file.

--- cmake/simple_features.cmake
+++ cmake/simple_features.cmake
@@ -2,6 +2,63 @@
 #This is being set here, because the check for iota in "check_include_file_cxx.cmake" is not working
 set (HAVE_IOTA true)
 
+
+if (EXISTS "${PROJECT_SOURCE_DIR}/src/DeviceModelPKG/ADMS" AND ( (NOT DEFINED Xyce_ADMS_MODELS) OR Xyce_ADMS_MODELS) )
+     set (Xyce_ADMS_MODELS TRUE CACHE BOOL "Include the ADMS directory, if it exists")
+     message(STATUS "Including the src/DeviceModelPKG/ADMS model directory")
+elseif (NOT DEFINED Xyce_ADMS_MODELS)
+     set (Xyce_ADMS_MODELS FALSE CACHE BOOL "Include the ADMS directory, if it exists")
+elseif (NOT Xyce_ADMS_MODELS)
+     message(STATUS "NOT including the src/DeviceModelPKG/ADMS model directory")
+     set (Xyce_ADMS_MODELS FALSE CACHE BOOL "Include the ADMS directory, if it exists")
+else ()
+     set (Xyce_ADMS_MODELS FALSE CACHE BOOL "Include the ADMS directory, if it exists")
+     message(WARNING "The src/DeviceModelPKG/ADMS directory is not in place - "
+          "changing Xyce_ADMS_MODELS to FALSE")
+endif ()
+
+if (EXISTS "${PROJECT_SOURCE_DIR}/src/DeviceModelPKG/NeuronModels" AND ( (NOT DEFINED Xyce_NEURON_MODELS) OR Xyce_NEURON_MODELS) )
+     set (Xyce_NEURON_MODELS TRUE CACHE BOOL "Include the Neuron directory, if it exists")
+     message(STATUS "Including the src/DeviceModelPKG/NeuronModels model directory")
+elseif (NOT DEFINED Xyce_NEURON_MODELS)
+     set (Xyce_NEURON_MODELS FALSE CACHE BOOL "Include the Neuron directory, if it exists")
+elseif (NOT Xyce_NEURON_MODELS)
+     set (Xyce_NEURON_MODELS FALSE CACHE BOOL "Include the Neuron directory, if it exists")
+     message(STATUS "NOT including the src/DeviceModelPKG/NeuronModels model directory")
+else ()
+     set (Xyce_NEURON_MODELS FALSE CACHE BOOL "Include the Neuron directory, if it exists")
+     message(WARNING "The src/DeviceModelPKG/NeuronModels directory is not in place - "
+          "changing Xyce_NEURON_MODELS to FALSE.")
+endif ()
+
+if (EXISTS "${PROJECT_SOURCE_DIR}/src/DeviceModelPKG/Xyce_NonFree" AND ( (NOT DEFINED Xyce_NONFREE_MODELS) OR Xyce_NONFREE_MODELS) )
+     set (Xyce_NONFREE_MODELS TRUE CACHE BOOL "Include the NonFree directory, if it exists")
+     message(STATUS "Including the src/DeviceModelPKG/Xyce_NonFree model directory")
+elseif (NOT DEFINED Xyce_NONFREE_MODELS)
+     set (Xyce_NONFREE_MODELS FALSE CACHE BOOL "Include the NonFree directory, if it exists")
+elseif (NOT Xyce_NONFREE_MODELS)
+     set (Xyce_NONFREE_MODELS FALSE CACHE BOOL "Include the NonFree directory, if it exists")
+     message(STATUS "NOT including the src/DeviceModelPKG/Xyce_NonFree model directory")
+else ()
+     set (Xyce_NONFREE_MODELS FALSE CACHE BOOL "Include the NonFree directory, if it exists")
+     message(WARNING "The src/DeviceModelPKG/NonFree directory is not in place - "
+          "changing Xyce_NONFREE_MODELS to FALSE.")
+endif ()
+
+if (EXISTS "${PROJECT_SOURCE_DIR}/src/DeviceModelPKG/SandiaModels" AND ( (NOT DEFINED Xyce_RAD_MODELS) OR Xyce_RAD_MODELS) )
+     set (Xyce_RAD_MODELS TRUE CACHE BOOL "Include the SandiaModels directory, if it exists")
+     message(STATUS "Including the src/DeviceModelPKG/SandiaModels model directory")
+elseif (NOT DEFINED Xyce_RAD_MODELS)
+     set (Xyce_RAD_MODELS FALSE CACHE BOOL "Include the SandiaModels directory, if it exists")
+elseif (NOT Xyce_RAD_MODELS)
+     message(STATUS "NOT including the src/DeviceModelPKG/SandiaModels model directory")
+     set (Xyce_RAD_MODELS FALSE CACHE BOOL "Include the SandiaModels directory, if it exists")
+else ()
+     set (Xyce_RAD_MODELS FALSE CACHE BOOL "Include the SandiaModels directory, if it exists")
+     message(WARNING "The src/DeviceModelPKG/SandiaModels directory is not in place - "
+          "changing Xyce_RAD_MODELS to FALSE.")
+endif ()
+
 #### Only on the systems that have bison and flex for now #######
 # Also this is both a C++ and CMAKE variable set. There are both ifs 
 # in the CmakeLists.txt in src/DeviceModelPKG/Core/CMakeLists.txt but 
