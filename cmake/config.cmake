af37408c709fee3c1078e4480aba73c21c5911c0
Evan Root <edroot@sandia.gov>
2019-10-02 18:21:14 -0600

    simple rename of simple_features to feature_modes.

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -2,6 +2,6 @@
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules")
 include(CheckIncludeFileCXX)
 include(CheckCXXSymbolExists)
-include(cmake/simple_features.cmake)
+include(cmake/feature_modes.cmake)
 include(cmake/tps.cmake)
 include(cmake/check_include_file_cxx.cmake)
