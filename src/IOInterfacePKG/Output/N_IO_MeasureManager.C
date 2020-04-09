941f766fa6e6687112a695475f47d467be5f7e40
Rich Schiek <rlschie@sandia.gov>
2020-04-09 09:19:23 -0600

    Replace deprecated mem_fun call with mem_fn
    In C++11 the std::mem_fun has been deprecated.  The new function
    to call is std::mem_fn.

--- src/IOInterfacePKG/Output/N_IO_MeasureManager.C
+++ src/IOInterfacePKG/Output/N_IO_MeasureManager.C
@@ -406,7 +406,7 @@ void Manager::updateTranMeasures(
   {
     (*it)->updateTran(comm, circuitTime, solnVec, stateVec, storeVec, lead_current_vector, junction_voltage_vector, lead_current_dqdt_vector);
   }
-  activeMeasuresList_.erase(std::remove_if(activeMeasuresList_.begin(), activeMeasuresList_.end(), std::mem_fun(&Measure::Base::finishedCalculation)),
+  activeMeasuresList_.erase(std::remove_if(activeMeasuresList_.begin(), activeMeasuresList_.end(), std::mem_fn(&Measure::Base::finishedCalculation)),
                             activeMeasuresList_.end());
 }
 
@@ -434,7 +434,7 @@ void Manager::updateDCMeasures(
   {
     (*it)->updateDC(comm, dcParamsVec, solnVec, stateVec, storeVec, lead_current_vector, junction_voltage_vector, lead_current_dqdt_vector);
   }
-  activeMeasuresList_.erase(std::remove_if(activeMeasuresList_.begin(), activeMeasuresList_.end(), std::mem_fun(&Measure::Base::finishedCalculation)),
+  activeMeasuresList_.erase(std::remove_if(activeMeasuresList_.begin(), activeMeasuresList_.end(), std::mem_fn(&Measure::Base::finishedCalculation)),
                             activeMeasuresList_.end());
 }
 
@@ -458,7 +458,7 @@ void Manager::updateACMeasures(
   {
     (*it)->updateAC(comm, frequency, real_solution_vector, imaginary_solution_vector, RFparams);
   }
-  activeMeasuresList_.erase(std::remove_if(activeMeasuresList_.begin(), activeMeasuresList_.end(), std::mem_fun(&Measure::Base::finishedCalculation)),
+  activeMeasuresList_.erase(std::remove_if(activeMeasuresList_.begin(), activeMeasuresList_.end(), std::mem_fn(&Measure::Base::finishedCalculation)),
                             activeMeasuresList_.end()); }
 
 //-----------------------------------------------------------------------------
