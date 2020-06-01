ef9676ddbfb103afd48e91841196fc4c5cfc36e6
Tom Russo <tvrusso@sandia.gov>
2020-06-01 08:47:56 -0600

    Update version string for QA stage

--- defaultConfig.cmake
+++ defaultConfig.cmake
@@ -11,10 +11,10 @@ project ( Xyce )
 set ( Xyce_VERSION_MAJOR           7                CACHE STRING " " )
 set ( Xyce_VERSION_MINOR           1                CACHE STRING " " )
 set ( Xyce_VERSION_PATCH           0                CACHE STRING " " )
-set ( Xyce_VERSION_EXTRA           "D"    CACHE STRING " " )
+set ( Xyce_VERSION_EXTRA           "Q"    CACHE STRING " " )
 
 # DEBUG:  rewrite the version code; be sure to use quotes
-set ( VERSION                      "R:D:7.1"         CACHE INTERNAL " " )
+set ( VERSION                      "R:Q:7.1"         CACHE INTERNAL " " )
 
 # set the top-level arch directory (eg. /Net/Proj/Xyce/arch/linux)
 # set ( Xyce_ARCHDIR                 "/Net/Proj/Xyce/arch/linux" )
