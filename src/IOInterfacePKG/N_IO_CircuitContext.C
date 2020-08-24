457c1689408c0207b7638440194f195a8359a223
Eric Keiter <erkeite@sandia.gov>
2020-08-24 15:50:45 -0600

    Fix spelling in an output message
    DEPRICATED -> DEPRECATED

--- src/IOInterfacePKG/N_IO_CircuitContext.C
+++ src/IOInterfacePKG/N_IO_CircuitContext.C
@@ -446,7 +446,7 @@ void CircuitContext::resolveQuote(Util::Param & parameter) const
 {
   if (parameter.isQuoted())
   {
-    Report::UserWarning() << "Automatic conversion of quoted filename to table is DEPRICATED "
+    Report::UserWarning() << "Automatic conversion of quoted filename to table is DEPRECATED "
       << " and will be removed in a future version of Xyce.  Please use the new syntax of tablefile(\"filename\") ";
     // The parameter is time dependent with its time history defined by the set
     // of time-value pairs in the file given by the value of the parameter.
