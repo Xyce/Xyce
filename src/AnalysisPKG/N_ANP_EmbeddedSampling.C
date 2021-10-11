212bb2866e285df75b491858baf8a4f382985a26
Tom Russo <tvrusso@sandia.gov>
2021-10-11 09:22:46 -0600

    Fix copyright headers
    A few files had outdated copyright ranges, and a few other files had
    an obsolete copyright block format (the current format for open source
    files is correctly found in the now-misnamed
    "src/headers/CVS-C++-File-Header-OS" and for ECI code in
    "src/headers/CVS-C++-File-Header-ECI"
    
    As near as I can tell these are the only files that needed updating
    for the 7.4 release, and they should have been caught for 7.3.
    
    In some cases, the difference in formatting could be responsible for
    them not being updated, because sometimes we do the updates using a
    regular expression replacement, and files with incorrectly formatted
    headers get missed.  In the case of the wildcard support stuff, it
    seems that the format is fine but somehow they got overlook,ed anyway.
    
    Xyce#312
    https://gitlab-ex.sandia.gov/Xyce/Xyce/-/issues/312

--- src/AnalysisPKG/N_ANP_EmbeddedSampling.C
+++ src/AnalysisPKG/N_ANP_EmbeddedSampling.C
@@ -1,26 +1,24 @@
-//-----------------------------------------------------------------------------
-// Copyright Notice
+//-------------------------------------------------------------------------
+//   Copyright 2002-2021 National Technology & Engineering Solutions of
+//   Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
+//   NTESS, the U.S. Government retains certain rights in this software.
 //
-//   Copyright 2002 Sandia Corporation. Under the terms
-//   of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
-//   Government retains certain rights in this software.
+//   This file is part of the Xyce(TM) Parallel Electronic Simulator.
 //
-//    Xyce(TM) Parallel Electrical Simulator
-//    Copyright (C) 2002-2017 Sandia Corporation
+//   Xyce(TM) is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
 //
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
+//   Xyce(TM) is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
 //
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//-----------------------------------------------------------------------------
+//   You should have received a copy of the GNU General Public License
+//   along with Xyce(TM).
+//   If not, see <http://www.gnu.org/licenses/>.
+//-------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
 // Purpose       : Embedded Sampling class analysis functions.
