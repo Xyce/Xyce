16b407a0aa84aab8fb2c5ff80fcad9c22f2eb290
Heidi Thornquist <hkthorn@sandia.gov>
2022-06-22 16:16:50 -0600

    Removes unused demangling code and fixes demangling
    There is several ifdefs in the demangling code for GNU and IBM
    compilers that is not used.  Therefore the strings passed to the
    Xyce::demangle method are not demangled.  There is a function
    in Teuchos::TypeNameTraits that can provide portable demangling
    of a type id.  So, Xyce::demangle simply calls the Teuchos function.
    
    Xyce/backlog/project-backlog#7

--- src/UtilityPKG/N_UTL_Demangle.C
+++ src/UtilityPKG/N_UTL_Demangle.C
@@ -37,102 +37,16 @@
 
 
 #include <N_UTL_Demangle.h>
-#include <stdlib.h>
 
-#if __GNUC__ == 3 || __GNUC__ == 4
-#include <cxxabi.h>
-#endif
-
-// #if defined __xlC__
-// #include <demangle.h>
-// #endif
+#include <Teuchos_TypeNameTraits.hpp>
 
 namespace Xyce {
 
-#ifdef Xyce__USE_PLATFORM_DEMANGLER
-
-#if defined(__GNUC__)
-
-#if (__GNUC__ == 3)
 std::string
 demangle(
   const char *	symbol)
 {
-#ifdef PURIFY_BUILD
-  return symbol;
-#else
-  std::string   s;
-  int		status = 0;
-
-  char *demangled_symbol = abi::__cxa_demangle(symbol, 0, 0, &status);
-
-  if (demangled_symbol) {
-    s = std::string(demangled_symbol);
-    free(demangled_symbol);
-  }
-
-  if (status != 0)
-    s = std::string(symbol);
-
-  return s;
-#endif
+  return Teuchos::demangleName( symbol );
 }
-
-#elif (__GNUC__ == 4)
-std::string
-demangle(
-  const char *	symbol)
-{
-#ifdef PURIFY_BUILD
-  return symbol;
-#else
-  std::string   s;
-
-  int		status;
-
-  char *demangled_symbol = __cxxabiv1::__cxa_demangle(symbol, 0, 0, &status);
-
-  if (demangled_symbol) {
-    s = std::string(demangled_symbol);
-    free(demangled_symbol);
-  }
-
-  if (status != 0)
-    s = std::string(symbol);
-
-  return s;
-#endif
-}
-
-#endif // (__GNUC__ == 3)
-
-#elif defined __xlC__
-std::string
-demangle(
-  const char *	symbol)
-{
-  return symbol;
-// #ifdef PURIFY_BUILD
-//   return symbol;
-// #else
-//   char *rest;
-
-//   Name *name = Demangle(symbol, rest) ;
-
-//   std::string s(name ? name->Text() : symbol);
-
-//   delete name;
-
-//   return s;
-// #endif
-}
-
-#endif // defined __GNUC__
-#else
-std::string demangle(const char *symbol) 
-{
-  return symbol;
-}
-#endif // Xyce__USE_PLATFORM_DEMANGLER
 
 } // namespace Xyce
