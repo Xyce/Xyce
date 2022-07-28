cb5d9d7f3f5e828d51227314855580962e8aeae4
Tom Russo <tvrusso@sandia.gov>
2022-07-28 13:05:24 -0600

    Add VT0 alias to MOS3
    Add an alias VT0 for VTO, and switch method of aliasing U0 and UO to
    the modern way instead of the 2001 way.
    
    Xyce/backlog/project-backlog#302

--- src/DeviceModelPKG/OpenModels/N_DEV_MOSFET3.C
+++ src/DeviceModelPKG/OpenModels/N_DEV_MOSFET3.C
@@ -172,6 +172,11 @@ void Traits::loadModelParameters(ParametricData<MOSFET3::Model> &p)
    .setCategory(CAT_VOLT)
    .setDescription("Zero-bias threshold voltage");
 
+  p.addPar ("VT0",0.0,&MOSFET3::Model::vt0)
+   .setUnit(U_VOLT)
+   .setCategory(CAT_VOLT)
+   .setDescription("Zero-bias threshold voltage (alias for VTO)");
+
   p.addPar ("KP",2e-5,&MOSFET3::Model::transconductance)
    .setUnit(U_AMPVM2)
    .setCategory(CAT_PROCESS)
@@ -286,10 +291,10 @@ void Traits::loadModelParameters(ParametricData<MOSFET3::Model> &p)
    .setCategory(ParameterCategory(CAT_PROCESS | UNDOCUMENTED))
    .setDescription("Surface mobility");
 
-  p.addPar ("U0",600.0,&MOSFET3::Model::surfaceMobility0)
+  p.addPar ("U0",600.0,&MOSFET3::Model::surfaceMobility)
    .setUnit(U_CMM2VM1SM1)
    .setCategory(CAT_PROCESS)
-   .setDescription("Surface mobility");
+   .setDescription("Surface mobility (alias for UO)");
 
   p.addPar ("FC",0.5,&MOSFET3::Model::fwdCapDepCoeff)
    .setUnit(U_NONE)
@@ -3169,7 +3174,7 @@ bool Model::processParams ()
                      CONSTQ * substrateDoping*1e6)/
           oxideCapFactor;
       }
-      if(!given("VTO"))
+      if(!given("VTO") && !given("VT0"))
       {
         if(!given("NSS"))
           surfaceStateDensity=0;
@@ -3315,16 +3320,6 @@ Model::Model(
   updateDependentParameters();
 
   // calculate dependent (ie computed) params and check for errors:
-  if (given("U0"))
-  {
-    if (given("UO"))
-      UserError(*this) << "Both uo and u0 have been specified and, which is not allowed";
-    else
-      UserWarning(*this) << "Surface mobility has been specified as u0 instead of uo, uo is the preferred syntax";
-
-    surfaceMobility = surfaceMobility0;
-  }
-
   processParams ();
 }
 
