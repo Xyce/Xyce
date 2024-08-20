ed20eca9a84d9fce5e433b816a23a201b8041feb
Gary L. Hennigan <glhenni@sandia.gov>
2024-08-20 12:47:54 -0600

    more lable testing

--- cmake/ctest/xyce-ctest.cmake
+++ cmake/ctest/xyce-ctest.cmake
@@ -180,7 +180,6 @@ if(buildReturnVal EQUAL 0)
   ctest_test(RETURN_VALUE testReturnVal
     PARALLEL_LEVEL $ENV{NUM_JOBS}
     INCLUDE_LABEL "^nightly"
-    INCLUDE_LABEL "^serial"
     EXCLUDE_LABEL "^required")
   if(VERBOSITY GREATER 1)
     message("[VERB1]: ctest_test() exited with return value: ${testReturnVal}")
