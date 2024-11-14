784b376761867b27764df77f08540245056cd60b
Rich Schiek <rlschie@sandia.gov>
2024-11-14 16:19:18 -0700

    Remove old scipt that isn't customized at installation
    https://cee-gitlab.sandia.gov/Xyce/backlog/project-backlog/-/issues/864

--- utils/XyceCInterface/MatlabExamples/SetupPythonEnv.m
+++
@@ -1,6 +0,0 @@
-% have to tell the python environment within matlab where to find xyce_interface 
-if count(py.sys.path,'/usr/local/Xyce_7.9/share') ==0 
-  insert(py.sys.path,int32(0),'/usr/local/Xyce_7.9/share')
-end
-
-
