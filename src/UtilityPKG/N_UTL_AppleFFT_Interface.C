df1bedccf8f70de48e7db94918b883ae897b5be9
Rich Schiek <rlschie@sandia.gov>
2025-01-22 16:53:53 -0700

    Remove unneeded code
    https://cee-gitlab.sandia.gov/Xyce/backlog/bugs/-/issues/40

--- src/UtilityPKG/N_UTL_AppleFFT_Interface.C
+++ src/UtilityPKG/N_UTL_AppleFFT_Interface.C
@@ -56,190 +56,93 @@
   template<>
   void N_UTL_AppleFFT_Interface<std::vector<double> >::calculateDFT()
   {
-    // Although we used a const on input to show that we aren't changing dftInData_
-    // we need to cast that away as the FFT library takes non-const pointers.
-    //std::vector<double>::const_iterator inDataItr = (this->dftInData_)->begin();
-    //double * inDataPtr = const_cast< double * >( &(*inDataItr) );
-    //std::vector<double>::iterator outResultItr = (this->dftOutData_)->begin();
-    //double * outResultPtr = &(*outResultItr);
-
-    //long status = DftiComputeForward( fftDescriptor, inDataPtr, outResultPtr);
-    //checkAndTrapErrors( status );
-    
-    // need to split interleaved input vector into two input vectors
-    // or if the input vector is 1/2 the length of the output vector 
-    // then leave the input as it is and make a zero vector of the same length 
-    //  Also need to two output vectors for the FFT results.
-    
-    /*
-    if( interleavedRoutines_ )
+    if(nextLargestPowerOf2_ == signalLength_)
     {
-      // copy dftInData_ to yTdInterleaved
+      // copy dftInData_ to yTdReal
       for( int i=0;i<nextLargestPowerOf2_;i++)
       {
         if( i<dftInData_->size())
         {
-          yTdInterleaved[2*i] = (*dftInData_)[i];
-          yTdInterleaved[2*i+1] = 0.0;
+          yTdReal[i] = (*dftInData_)[i];
         }
         else
         {
-          yTdInterleaved[2*i] = 0.0;
-          yTdInterleaved[2*i+1] = 0.0;
+          yTdReal[i] = 0.0;
         }
+        yTdImag[i] = 0.0;
       }
-      vDSP_DFT_Interleaved_ExecuteD(forwardInterleavedSetup_, (DSPDoubleComplex*)(yTdInterleaved.data()), (DSPDoubleComplex*)(yFdInterleaved.data()));
       
+      vDSP_DFT_ExecuteD(forwardSetup_, yTdReal.data(), yTdImag.data(), yFdReal.data(), yFdImag.data());
+    
+      // now interleave the output vectors for return.
       for( int i=0;i<nextLargestPowerOf2_;i++)
       {
-        if( i<dftOutData_->size())
+        if( (2*i+1)<dftOutData_->size())
         {
-          // magnitude of frequency domain values here are a factor of 2 larger
-          // than the results from FFTW.  Factor out that 2 here for consistency.
-          (*dftOutData_)[i] = yFdInterleaved[i]/2.0;
+          (*dftOutData_)[2*i] = yFdReal[i];
+          (*dftOutData_)[2*i+1] = yFdImag[i];
         }
       }
-      // yFdInterleaved[0] is the DC component and yFdInterleaved[1] 
-      // is the Nyquist component.  Move the Nyquist component to the 
-      // last real position.  Zero the original spot where the nyquist value was stored.
-      //(*dftOutData_)[nextLargestPowerOf2_] = (*dftOutData_)[1];
-      (*dftOutData_)[1] = 0.0;
     }
     else
     {
-    */
-      
-      if(nextLargestPowerOf2_ == signalLength_)
+      for( auto k=0; k<(1+signalLength_/2); k++)
       {
-        // copy dftInData_ to yTdReal
-        for( int i=0;i<nextLargestPowerOf2_;i++)
+        (*dftOutData_)[2*k] = 0.0;
+        (*dftOutData_)[2*k+1] = 0.0;
+        for( auto j=0; j<signalLength_; j++)
         {
-          if( i<dftInData_->size())
-          {
-            yTdReal[i] = (*dftInData_)[i];
-          }
-          else
-          {
-            yTdReal[i] = 0.0;
-          }
-          yTdImag[i] = 0.0;
-        }
-        
-        vDSP_DFT_ExecuteD(forwardSetup_, yTdReal.data(), yTdImag.data(), yFdReal.data(), yFdImag.data());
-      
-        // now interleave the output vectors for return.
-        for( int i=0;i<nextLargestPowerOf2_;i++)
-        {
-          if( (2*i+1)<dftOutData_->size())
-          {
-            (*dftOutData_)[2*i] = yFdReal[i];
-            (*dftOutData_)[2*i+1] = yFdImag[i];
-          }
+          (*dftOutData_)[2*k] += std::cos(-2*M_PI*j*k/signalLength_)*(*dftInData_)[j];
+          (*dftOutData_)[2*k+1] += std::sin(-2*M_PI*j*k/signalLength_)*(*dftInData_)[j];
         }
       }
-      else
-      {
-        for( auto k=0; k<(1+signalLength_/2); k++)
-        {
-          (*dftOutData_)[2*k] = 0.0;
-          (*dftOutData_)[2*k+1] = 0.0;
-          for( auto j=0; j<signalLength_; j++)
-          {
-            (*dftOutData_)[2*k] += std::cos(-2*M_PI*j*k/signalLength_)*(*dftInData_)[j];
-            (*dftOutData_)[2*k+1] += std::sin(-2*M_PI*j*k/signalLength_)*(*dftInData_)[j];
-          }
-        }
-      }
-    //}
+    }
   }
 
   // Calculate IFT with the vectors that have been registered.
   template<>
   void N_UTL_AppleFFT_Interface<std::vector<double> >::calculateIFT()
   {
-    // Although we used a const on input to show that we aren't changing iftInData_
-    // we need to cast that away as the FFT library takes non-const pointers.
-    //std::vector<double>::const_iterator inDataItr = (this->iftInData_)->begin();
-    //double * inDataPtr = const_cast< double * >( &(*inDataItr) );
-    //std::vector<double>::iterator outResultItr = (this->iftOutData_)->begin();
-    //double * outResultPtr = &(*outResultItr);
-
-    //long status = DftiComputeBackward( fftDescriptor, inDataPtr, outResultPtr);
-    //checkAndTrapErrors( status );
-    
-    // need to break the interleaved input vector into two vectors 
-    // make destination arrays for the real and imaginary results 
-    
-    /*
-    if( interleavedRoutines_ )
+    if(nextLargestPowerOf2_ == signalLength_)
     {
-      for( int i=0;i<(2*nextLargestPowerOf2_);i++)
+      // copy iftInData_ to yFdReal and yFdImag
+      for( int i=0;i<(nextLargestPowerOf2_/2);i++)
       {
-        if( i<iftInData_->size())
+        if( (2*i)<iftInData_->size())
         {
-          yFdInterleaved[i] = (*iftInData_)[i];
+          yFdReal[i] = (*iftInData_)[2*i];
+          yFdImag[i] = (*iftInData_)[2*i+1];
         }
         else
         {
-          yFdInterleaved[i] = 0.0;
+          yFdReal[i] = 0.0;
+          yFdImag[i] = 0.0;
         }
       }
-      
-      vDSP_DFT_Interleaved_ExecuteD(inverseInterleavedSetup_, (DSPDoubleComplex*)(yFdInterleaved.data()), (DSPDoubleComplex*)(yTdInterleaved.data()));
-      
+      vDSP_DFT_ExecuteD(inverseSetup_, yFdReal.data(), yFdImag.data(), yTdReal.data(), yTdImag.data() );
+      // now combine the output arrays into an interleaved result.
       for( int i=0;i<nextLargestPowerOf2_;i++)
       {
         if( i<iftOutData_->size())
         {
-          (*iftOutData_)[i] = scaleFactor_ * yTdInterleaved[2*i];
+          (*iftOutData_)[i] = scaleFactor_ * yTdReal[i];
         }
-      } 
-      
+      }
     }
     else
     {
-    */
-      if(nextLargestPowerOf2_ == signalLength_)
+      for( auto k=0; k<signalLength_; k++)
       {
-        // copy iftInData_ to yFdReal and yFdImag
-        for( int i=0;i<(nextLargestPowerOf2_/2);i++)
+        (*iftOutData_)[k] = 0.0;
+        for( auto j=0; j<((signalLength_/2)+1); j++)
         {
-          if( (2*i)<iftInData_->size())
-          {
-            yFdReal[i] = (*iftInData_)[2*i];
-            yFdImag[i] = (*iftInData_)[2*i+1];
-          }
-          else
-          {
-            yFdReal[i] = 0.0;
-            yFdImag[i] = 0.0;
-          }
+          (*iftOutData_)[k] += (std::cos(2*M_PI*j*k/signalLength_))*(*iftInData_)[2*j] - (std::sin(2*M_PI*j*k/signalLength_))*(*iftInData_)[2*j+1];
         }
-        vDSP_DFT_ExecuteD(inverseSetup_, yFdReal.data(), yFdImag.data(), yTdReal.data(), yTdImag.data() );
-        // now combine the output arrays into an interleaved result.
-        for( int i=0;i<nextLargestPowerOf2_;i++)
+        for( auto j=(signalLength_/2), jc=(signalLength_/2)+1; j>0; j--, jc++)
         {
-          if( i<iftOutData_->size())
-          {
-            (*iftOutData_)[i] = scaleFactor_ * yTdReal[i];
-          }
+          (*iftOutData_)[k] += (std::cos(2*M_PI*jc*k/signalLength_))*(*iftInData_)[2*j] + (std::sin(2*M_PI*jc*k/signalLength_))*(*iftInData_)[2*j+1];
         }
+        (*iftOutData_)[k] = (*iftOutData_)[k] / signalLength_;
       }
-      else
-      {
-        for( auto k=0; k<signalLength_; k++)
-        {
-          (*iftOutData_)[k] = 0.0;
-          for( auto j=0; j<((signalLength_/2)+1); j++)
-          {
-            (*iftOutData_)[k] += (std::cos(2*M_PI*j*k/signalLength_))*(*iftInData_)[2*j] - (std::sin(2*M_PI*j*k/signalLength_))*(*iftInData_)[2*j+1];
-          }
-          for( auto j=(signalLength_/2), jc=(signalLength_/2)+1; j>0; j--, jc++)
-          {
-            (*iftOutData_)[k] += (std::cos(2*M_PI*jc*k/signalLength_))*(*iftInData_)[2*j] + (std::sin(2*M_PI*jc*k/signalLength_))*(*iftInData_)[2*j+1];
-          }
-          (*iftOutData_)[k] = (*iftOutData_)[k] / signalLength_;
-        }
-      }
-    //}
+    }
   }
